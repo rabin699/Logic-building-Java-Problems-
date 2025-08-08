9ce912381f63d26175520ff1f382c2d929db9012
rabin699 <rabinhumagain444@gmail.com>
2025-08-08 19:41:11 +0545

    solved problem 005

--- Problem005.java
+++ Problem005.java
@@ -1,2 +1,34 @@
 // Problem 005: Write a program to calculate Simple Interest and Compound
 // Interest.
+
+import java.util.Scanner;
+
+public class Problem005 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        float principle, interest, rate;
+        int time = 0;
+        System.out.println("Enter the amount(Nrs.) rate(%)");
+        principle = scn.nextFloat();
+        rate = scn.nextFloat();
+
+        System.out.println("""
+                    Time:
+                    1.Months(Press 1)
+                    2.Year(Press 2)
+                """);
+        int option = scn.nextInt();
+        if (option == 1) {
+            System.out.println("Enter your time in months:");
+            time = scn.nextInt();
+            interest = (float) (principle * time / 12.0 * rate) / 100;
+            System.out.println("Calculated interest in " + time + " months is Nrs. " + interest);
+        }
+        if (option == 2) {
+            interest = (float) (principle * time * rate) / 100;
+            System.out.println("Calculated interest in " + time + " years is Nrs. " + interest);
+        }
+        scn.close();
+    }
+
+}
