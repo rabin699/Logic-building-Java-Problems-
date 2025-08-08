7f92d9e1c54417dd3d48864721015ee685ed9253
rabin699 <rabinhumagain444@gmail.com>
2025-08-08 19:20:34 +0545

    solved problem 004

--- Problem004.java
+++ Problem004.java
@@ -1,1 +1,24 @@
 // Problem 004: Write a program to calculate volume of a cylinder.
+
+import java.util.Scanner;
+
+public class Problem004 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        double radious, height;
+        System.out.println("""
+                 _______
+                |       |  |
+                |       |  |
+                |       |  |-->  Height
+                |_______|  |
+                    ---->radious
+                """);
+        System.out.println("Enter the radious and height of cylinder");
+        radious = scn.nextDouble();
+        height = scn.nextDouble();
+        double volume = Math.PI * Math.pow(radious, 2) * height;
+        System.out.println("The volume of cylinder of height " + height + " and radious " + radious + " is " + volume);
+        scn.close();
+    }
+}
