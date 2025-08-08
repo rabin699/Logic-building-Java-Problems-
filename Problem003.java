615294f45b26eb1f4d879478670b916a29031332
rabin699 <rabinhumagain444@gmail.com>
2025-08-08 19:10:35 +0545

    solved probelm 003

--- Problem003.java
+++ Problem003.java
@@ -1,1 +1,17 @@
 // Problem 003: Write a program to find area and circumference of a circle.
+
+import java.util.Scanner;
+
+public class Problem003 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        float radious, area, circumference;
+        System.out.println("Enter the radious of the circle:");
+        radious = scn.nextFloat();
+        area = (float) Math.PI * (float) Math.pow(radious, 2);
+        circumference = (float) Math.PI * 2 * radious;
+        System.out.println("Circle of radious " + radious + " has area: " + area);
+        System.out.println("Circle of radious " + radious + " has circumference: " + circumference);
+        scn.close();
+    }
+}
