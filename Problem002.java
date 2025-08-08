e093b0e3ed9b3bb95a932cac024d9b7801153b7c
rabin699 <rabinhumagain444@gmail.com>
2025-08-08 19:04:23 +0545

    solved problem 002

--- Problem002.java
+++ Problem002.java
@@ -1,6 +1,15 @@
 // Problem 002: Write a program to convert Fahrenheit to Celsius.
+
+import java.util.Scanner;
+
 public class Problem002 {
     public static void main(String[] args) {
-        
+        Scanner scn = new Scanner(System.in);
+        float fahrenheit, celsius;
+        System.out.println("Enter Fahrenheit Temperature: ");
+        fahrenheit = scn.nextFloat();
+        celsius = (float) 9 / 5 * fahrenheit + 32; // (explicit conversion to float using (float))
+        System.out.println(fahrenheit + " fahrenheit in celcious is " + celsius);
+        scn.close();
     }
 }
