46a98ce2c07f42e5856d5cb9e887f2c5ff50c677
rabin699 <rabinhumagain444@gmail.com>
2025-08-09 07:13:31 +0545

    solved problem 006

--- Problem006.java
+++ Problem006.java
@@ -1,1 +1,19 @@
 // Problem 006: Write a program to swap two numbers using a third variable.
+
+import java.util.Scanner;
+
+public class Problem006 {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter two numbers :");
+        int a = scn.nextInt();
+        int b = scn.nextInt();
+        System.out.println("The original values are A:" + a + " B:" + b);
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println("The changed values are A:" + a + " B:" + b);
+        scn.close();
+    }
+}
