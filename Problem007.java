8ea012be0567ab97ad978c1638fb034a3aba5e75
rabin699 <rabinhumagain444@gmail.com>
2025-08-09 07:19:53 +0545

    solved problem 007

--- Problem007.java
+++ Problem007.java
@@ -1,1 +1,16 @@
 // Problem 007: Write a program to evaluate cube of (a + b).
+
+import java.util.Scanner;
+
+public class Problem007 {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter two numbers to find their (a+b)^3");
+        int a = scn.nextInt();
+        int b = scn.nextInt();
+        double calculateCube = Math.pow(a, 3) + Math.pow(b, 3) + 3 * b * Math.pow(a, 2) + 3 * a * Math.pow(b, 2);
+        System.out.println("The cube of (a+b) is: " + calculateCube);
+        scn.close();
+    }
+}
