56b2cd273b70212fb28ec6085d97a80976c4b6b5
rabin699 <rabinhumagain444@gmail.com>
2025-08-10 14:43:29 +0545

    solved problem 015

--- Problem015.java
+++ Problem015.java
@@ -1,1 +1,22 @@
 // Problem 015: Write a program to find the largest among 3 numbers.
+
+import java.util.Scanner;
+
+public class Problem015 {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter three numbers : ");
+        int a = scn.nextInt();
+        int b = scn.nextInt();
+        int c = scn.nextInt();
+        if (a > b && a > c) {
+            System.out.println(a + " is largest among three numbers.");
+        } else if (b > c) {
+            System.out.println(b + " is largest among three numbers.");
+        } else {
+            System.out.println(c + " is largest among three numbers.");
+        }
+        scn.close();
+    }
+}
