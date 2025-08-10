2d6e9e5ea8afa34f51da492d2e8608e273eba633
rabin699 <rabinhumagain444@gmail.com>
2025-08-10 14:38:54 +0545

    solved problem 012

--- Problem012.java
+++ Problem012.java
@@ -1,1 +1,24 @@
 // Problem 012: Write a program to check whether a number is even or odd.
+
+import java.util.Scanner;
+
+public class Problem012 {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int number;
+        System.out.println("Enter a number to check if its even or odd");
+        number = scn.nextInt();
+        scn.close();
+        if (number <= 0) {
+            System.out.println("The number is neither odd nor even " + number);
+            return;
+        }
+        if (number % 2 == 0) {
+            System.out.println("The number is even: " + number);
+        } else {
+            System.out.println("The number is odd: " + number);
+        }
+
+    }
+}
