1fe514e82100947c4e2022804db001608a2f0fd2
rabin699 <rabinhumagain444@gmail.com>
2025-08-11 19:50:23 +0545

    solved problem 013

--- Problem013.java
+++ Problem013.java
@@ -1,1 +1,31 @@
 // Problem 013: Write a program to check if a number is an Armstrong number.
+
+import java.util.Scanner;
+
+public class Problem013 {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter a number please:");
+        int num = scn.nextInt();
+        int temp = num, tem1 = num, pow = 0;
+        int isArmstrong = 0;
+        while (temp != 0) {
+            temp /= 10;
+            pow++;
+        }
+        while (tem1 != 0) {
+            int remainder = tem1 % 10;
+            isArmstrong += Math.pow(remainder, pow);
+            tem1 /= 10;
+        }
+        if (isArmstrong == num) {
+            System.out.println("The number is Armstrong:" + num);
+        } else {
+            System.out.println("The number is not Armstrong:" + num);
+        }
+        scn.close();
+
+    }
+
+}
