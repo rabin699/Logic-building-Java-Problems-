1403ec7ebe8aec8dfce114f4f40437978de17b8e
rabin699 <rabinhumagain444@gmail.com>
2025-08-11 20:03:25 +0545

    solved problem 016

--- Problem016.java
+++ Problem016.java
@@ -1,1 +1,23 @@
 // Problem 016: Write a program to assign grades based on marks.
+
+import java.util.Scanner;
+
+public class Problem016 {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter the marks of student:");
+        float marks = scn.nextFloat();
+        switch ((int) marks / 10) {
+            case 10 -> System.out.println("Excellent 100 out of 100 wow");
+            case 9 -> System.out.println("A+ grade");
+            case 8 -> System.out.println("A grade");
+            case 7 -> System.out.println("B+ grade");
+            case 6 -> System.out.println("B grade");
+            case 5 -> System.out.println("C+ grade");
+            case 4 -> System.out.println("C grade");
+            default -> System.out.println("Failed af");
+        }
+        scn.close();
+    }
+}
