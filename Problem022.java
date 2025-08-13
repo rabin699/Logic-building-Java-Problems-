bbc96021d0827d19f7355881667b5fc875f356ab
rabin699 <rabinhumagain444@gmail.com>
2025-08-13 16:14:00 +0545

    solved problem 022

--- Problem022.java
+++ Problem022.java
@@ -1,1 +1,16 @@
 // Problem 022: Write a program to print multiplication table of a number.
+
+import java.util.Scanner;
+
+public class Problem022 {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter a number for it's multiplication table:");
+        int num = scn.nextInt();
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(num + " x " + i + " = " + num * i);
+        }
+        scn.close();
+    }
+}
