c413e7f82ec84383fa85ff40640abd38e262a0df
rabin699 <rabinhumagain444@gmail.com>
2025-08-13 15:45:55 +0545

    solved problem 017

--- Problem017.java
+++ Problem017.java
@@ -1,1 +1,18 @@
 // Problem 017: Write a program to check whether a year is a leap year.
+
+import java.util.Scanner;
+
+public class Problem017 {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter the year to check if it is leap year:");
+        int year = scn.nextInt();
+        if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)) {
+            System.out.println("The year is leap year ");
+        } else {
+            System.out.println("The number is not a leap year ");
+        }
+        scn.close();
+    }
+}
