b8cb44c87109bd807264e71d5f8798f1f7484058
rabin699 <rabinhumagain444@gmail.com>
2025-08-13 15:46:27 +0545

    solved problem 020

--- Problem020.java
+++ Problem020.java
@@ -1,1 +1,31 @@
 // Problem 020: Write a program to check if a number is prime.
+
+import java.util.Scanner;
+
+public class Problem020 {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter the number to check:");
+        int num = scn.nextInt();
+        boolean isPrime = true;
+        if (num <= 1) {
+            System.out.println("The number is not prime ");
+            scn.close();
+            return;
+        }
+
+        for (int i = 2; i <= num / 2; i++) {
+            if (num % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        if (isPrime) {
+            System.out.println("The number is prime ");
+        } else {
+            System.out.println("The number is not prime ");
+        }
+        scn.close();
+    }
+}
