70e60cd0f33602da4f0cac96c91e362e321cbbf5
rabin699 <rabinhumagain444@gmail.com>
2026-02-18 21:21:32 +0545

    solved problem 26

--- Problem026.java
+++ Problem026.java
@@ -1,1 +1,14 @@
 // Problem 026: Write a program to count persons aged between 50 to 60.
+class Problem026{
+  public static void main(String[] args) {
+    int[] array={20,30 ,50,80,90,100,65,22,55,70};
+    //or we can take a user input too but i wont do this okay
+    int count=0;
+    for (int i : array) {
+      if(i>=50 && i<=60){
+        count++;
+      }
+    }
+    System.out.println("The person aged between 50 to 60 are :"+count);
+  }
+}
